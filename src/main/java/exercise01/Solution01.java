4ca9e2f4d45515955b40aab3fa92dca9b6ecb2fd
cristiamenciso <cristiam.enciso@gmail.com>
2021-09-05 16:44:10 -0400

    Initial Solution 01

--- src/main/java/exercise01/Solution01.java
+++ src/main/java/exercise01/Solution01.java
@@ -1,5 +1,23 @@
 package exercise01;
 
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 first_name last_name
+ */
+/*
+Exercise 1 - Saying Hello
+        Create a program that prompts for your name and prints a greeting using your name.
+
+        Example Output
+        What is your name? Brian
+        Hello, Brian, nice to meet you!
+        Constraint
+        Perform the input, string concatenation, and output in separate statements.
+        Challenges
+        Write a new version of the program without using any variables.
+        Write a version of the program that displays different greetings for different people.
+        Write a version of the program that displays the greeting in a GUI window.
+*/
 import java.util.Scanner;
 // create a scanner object
 // Prompt user for their name
